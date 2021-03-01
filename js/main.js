3bcf7f02fccefc6375dc673b82515d914c41f4ce
hangyolman <gksrufaos@gmail.com>
2021-03-01 21:25:04 +0900

    포트폴리오 2차 반응형 완성본 - 스크롤매직 수정

--- js/main.js
+++ js/main.js
@@ -11,109 +11,29 @@ $(function () { // wait for document ready
     }
   });
 
-  //The GreenSock Animation Platform (GSAP)
-  //Tweens 란 GSAP의 기본적인 애니메이션
-  // var tween1 = TweenMax.to('#animate1', 3, {
-  //   opacity:0,
-  //   backgroundColor:"red",
-  //   ease: Power4.easeOut
-  // }).from('#animate1', 2, {
-  //   opacity:1
-  // });
+  var timeline = new TimelineMax();
 
-
-
-  // var tween1 = TweenMax.to("#animate1", 5, {
-  //   opacity:0,
-  //   backgroundColor:"#000",
-  //   ease: Power4.easeOut
-  // }).to("#animate1", 1, {
-  //   scaleX: 1.5,
-  //   scaleY: 1.5,
-  //   backgroundColor:"#454545",
-  //   ease: Back.easeOut
-  // });
-  // var $txt = $("#trigger1 .txt");
-  // $txt.lettering();
-
-  // ani();
-
-  // function ani(){
-  //   var tween1 = TweenMax.staggerFromTo($("#trigger1 span"), 0.5, {
-  //       ease: Back.easeOut.config(1.7),
-  //       opacity: 0,
-  //       bottom: -100
-  //     }, {
-  //       ease: Back.easeOut.config(1.7),
-  //       opacity: 1,
-  //       bottom: 0
-  //     }, 0.05
-  //   );
-  // }
-
- var timeline = new TimelineMax();
-
- var tween1 = TweenMax.staggerFromTo($(".top__inner"), 5, {
-    ease: Back.easeOut.config(1, 0.3),
-    opacity: 1,
-    scaleX:1.5,
-    scaleY:1.5
-  }, {
-    ease: Back.easeOut.config(1, 0.3),
-    opacity: 0,
-    scaleX:1,
-    scaleY:1 
-  }
-);
-
-// var tween2 = TweenMax.fromTo($("trigger1__txt2"), 2, {
-//     ease: Back.easeOut.config(1, 0.3),
-//     opacity: 0
-//   }, {
-//     ease: Back.easeOut.config(1, 0.3),
-//     opacity: 1
-//   }
-// );
-
-// var tween3 = TweenMax.to($("#trigger1 .txt2"), 10, {
-//     ease: Back.easeOut.config(1, 0.3),
-//     opacity: 0    
-//   }
-// );
-
-  // var tween2 = TweenMax.from('#animate2', 1, {
-  //   opacity:1,
-  //   backgroundColor:"blue",
-  // })
-
-  // get all slides
-  // var slides = document.querySelectorAll(".contents");
-
-  // create scene for every slide
-  // for (var i=0; i<slides.length; i++) {
-  //   var scene1 = new ScrollMagic.Scene({
-  //       triggerElement: slides[i]
-  //     })
-  //     .setPin(slides[i], {pushFollowers: false})
-  //     .addIndicators({
-  //       suffix:0,
-  //       colorTrigger: "transparent", //트리거 팁 색상 
-  //       colorStart: "transparent", //스타트 팁 색상 
-  //       colorEnd: "transparent", //종료 팁 색상,
-  //       indent:0
-  //     })
-  //     .addTo(controller);
-  // }
+  var tween1 = TweenMax.staggerFromTo($(".top__inner"), 5, {
+      ease: Back.easeOut.config(1, 0.3),
+      opacity: 1,
+      scaleX:1.5,
+      scaleY:1.5
+    }, {
+      ease: Back.easeOut.config(1, 0.3),
+      opacity: 0,
+      scaleX:1,
+      scaleY:1 
+    }
+  );
 
   var scene2 = new ScrollMagic.Scene({
-    triggerElement: "#top"
+    triggerElement: ".top"
   })
   timeline.add(tween1);
   //.add(tween2).add(tween3)
-  scene2.setPin("#top", {pushFollowers: false})
+  scene2.setPin(".top", {pushFollowers: false})
   scene2.setTween(timeline)
   scene2.addTo(controller)
-
 
 
   var typed = new Typed("#typed_string", {
